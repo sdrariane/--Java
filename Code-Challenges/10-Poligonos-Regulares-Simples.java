355a467a9459c0d66ddbffa557963f26cf822854
Isadora Ariane <65814808+sdrariane@users.noreply.github.com>
2022-05-15 11:38:35 -0300

    Add files via upload

---
+++ Code-Challenges/10-Poligonos-Regulares-Simples.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    var scan = new Scanner(System.in);
+    int N = scan.nextInt();
+    int L = scan.nextInt();
+    int P = N * L;
+    System.out.println(P);
+  }
+}
